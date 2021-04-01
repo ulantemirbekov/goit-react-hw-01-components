7e2b7e142e2a4d49c6635dbcf5e223b9ebae6e8f
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-01 22:58:44 +0300

    Statistics component rendered

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+
 import Profile from './components/Profile/Profile';
 import user from './data/user.json';
+
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './data/statistical-data.json';
+
 
 const App = () => {
     return (
@@ -12,6 +17,12 @@ const App = () => {
                 avatar={user.avatar}
                 stats={user.stats}
             />
+
+            <Statistics
+                title="Upload stats"
+                stats={statisticalData}
+            />
+            {/* <Statistics stats={statisticalData} /> */}
         </div>
     );
 };
