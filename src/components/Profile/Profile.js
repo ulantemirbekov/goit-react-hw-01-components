2d0770077952f44f8b81d71b6ef0ca7a02c181fd
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-01 21:02:16 +0300

    Profile component added

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+    return (
+        <div className="profile">
+            <div className="description">
+                <img
+                    src={avatar}
+                    alt="Аватар пользователя"
+                    className="avatar"
+                    width="140"
+                />
+                <p className="name">{name}</p>
+                <p className="tag">{tag}</p>
+                <p className="location">{location}</p>
+            </div>
+
+            <ul className="stats">
+                <li>
+                    <span className="label">Followers</span>
+                    <span className="quantity">{stats.followers}</span>
+                </li>
+                <li>
+                    <span className="label">Views</span>
+                    <span className="quantity">{stats.views}</span>
+                </li>
+                <li>
+                    <span className="label">Likes</span>
+                    <span className="quantity">{stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Profile;
