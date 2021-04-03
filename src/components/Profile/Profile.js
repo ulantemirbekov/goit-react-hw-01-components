bf7da5ee59a12a99b0efee6cbfd4e005b4f0784e
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-03 13:36:59 +0300

    defaultProps and propTypes added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import defaultAvatar from './defaultAvatar.jpg';
+import styles from './Profile.module.css';
+
 
 const Profile = ({ name, tag, location, avatar, stats }) => {
     return (
@@ -6,31 +9,46 @@ const Profile = ({ name, tag, location, avatar, stats }) => {
             <div className="description">
                 <img
                     src={avatar}
-                    alt="Аватар пользователя"
-                    className="avatar"
-                    width="140"
+                    alt={"Аватар " + name}
+                    className={styles.avatar}
                 />
                 <p className="name">{name}</p>
                 <p className="tag">{tag}</p>
                 <p className="location">{location}</p>
             </div>
 
-            <ul className="stats">
-                <li>
+            <ul className={styles.stats}>
+                <li className={styles.stats_item}>
                     <span className="label">Followers</span>
-                    <span className="quantity">{stats.followers}</span>
+                    <span className={styles.quantity}>{stats.followers}</span>
                 </li>
-                <li>
+                <li className={styles.stats_item}>
                     <span className="label">Views</span>
-                    <span className="quantity">{stats.views}</span>
+                    <span className={styles.quantity}>{stats.views}</span>
                 </li>
-                <li>
+                <li className={styles.stats_item}>
                     <span className="label">Likes</span>
-                    <span className="quantity">{stats.likes}</span>
+                    <span className={styles.quantity}>{stats.likes}</span>
                 </li>
             </ul>
         </div>
     );
+};
+
+Profile.defaultProps = {
+    avatar: defaultAvatar,
+};
+
+Profile.propTypes = {
+    name: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    stats: {
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+    }
 };
 
 export default Profile;
