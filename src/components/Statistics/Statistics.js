44ac86ff4be81571657fc7798e6ca6c1b09f6528
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-03 15:16:45 +0300

    render of title changed and propTypes added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
 
-const Statistics = ({ title, stats }) => {
+const Statistics = ({ title = "", stats }) => {
     return (
         <section className="statistics">
-            <h2 className="title">{title}</h2>
+            {title && <h2 className="title">{title}</h2>}
 
-            <ul className="stat-list">
+            <ul className={styles.stat_list}>
                 {stats.map(item => (
-                    <li className="item">
+                    <li className={styles.stat_item} key={item.id}>
                         <span className="label">{item.label}</span>
-                        <span className="percentage">{item.percentage}</span>
+                        <span className={styles.item_percentage}>{item.percentage}%</span>
                     </li>
                 ))}
             </ul>
         </section>
     );
+};
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired
+        })
+    ).isRequired
 };
 
 export default Statistics;
