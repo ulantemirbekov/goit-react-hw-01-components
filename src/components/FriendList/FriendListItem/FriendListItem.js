9ff2f271741a172385968b9b5eab12f146b64de9
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-05 00:20:22 +0300

    FriendListItem.module.css updated

--- src/components/FriendList/FriendListItem/FriendListItem.js
+++ src/components/FriendList/FriendListItem/FriendListItem.js
@@ -7,7 +7,7 @@ const FriendListItem = ({ avatar = "", name, isOnline }) => {
         <li className={styles.item}>
             <span className={isOnline ? styles.online : styles.offline}></span>
             <img className={styles.avatar} src={avatar} alt={"Аватар " + name} width="48" />
-            <p className="name">{name}</p>
+            <p className={styles.name}>{name}</p>
         </li>
     )
 };
