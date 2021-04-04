adb9e3ea9679b7ff1cd2078835bef21f0521576b
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-04 18:06:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-
 import Profile from './components/Profile/Profile';
 import user from './data/user.json';
 
 import Statistics from './components/Statistics/Statistics';
 import statisticalData from './data/statistical-data.json';
 
-import FriendList from './components/Friends/FriendList';
+import FriendList from './components/FriendList/FriendList';
 import friends from './data/friends.json';
+
+import TransactionHistory from './components/Transactions/TransactionHistory';
+import transactions from './data/transactions.json';
 
 
 const App = () => {
@@ -25,9 +26,11 @@ const App = () => {
                 title="Upload stats"
                 stats={statisticalData}
             />
-            {/* <Statistics stats={statisticalData} /> */}
+            <Statistics stats={statisticalData} />
 
             <FriendList friends={friends} />
+
+            <TransactionHistory items={transactions} />
         </div>
     );
 };
