c8aaf32a0be1dfe91f78f95f19828d0b48444c01
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-05 00:18:46 +0300

    Profile.module.css updated

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,29 +5,29 @@ import styles from './Profile.module.css';
 
 const Profile = ({ name, tag, location, avatar, stats }) => {
     return (
-        <div className="profile">
-            <div className="description">
+        <div className={styles.profile}>
+            <div className={styles.description}>
                 <img
                     src={avatar}
                     alt={"Аватар " + name}
                     className={styles.avatar}
                 />
-                <p className="name">{name}</p>
-                <p className="tag">{tag}</p>
-                <p className="location">{location}</p>
+                <p className={styles.name}>{name}</p>
+                <p className={styles.tag}>{tag}</p>
+                <p className={styles.location}>{location}</p>
             </div>
 
             <ul className={styles.stats}>
                 <li className={styles.stats_item}>
-                    <span className="label">Followers</span>
+                    <span className={styles.label}>Followers</span>
                     <span className={styles.quantity}>{stats.followers}</span>
                 </li>
                 <li className={styles.stats_item}>
-                    <span className="label">Views</span>
+                    <span className={styles.label}>Views</span>
                     <span className={styles.quantity}>{stats.views}</span>
                 </li>
                 <li className={styles.stats_item}>
-                    <span className="label">Likes</span>
+                    <span className={styles.label}>Likes</span>
                     <span className={styles.quantity}>{stats.likes}</span>
                 </li>
             </ul>
